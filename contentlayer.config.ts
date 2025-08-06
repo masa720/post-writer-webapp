bfacacb7b56ad07fdaa79c79f379b5c62e63139a
Masaki <>
2025-08-06 14:32:13 -0700

    add: install contentLayer2

---
+++ contentlayer.config.ts
@@ -0,0 +1,38 @@
+import { defineDocumentType, makeSource } from "contentlayer2/source-files";
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `blog/**/*.mdx`,
+  fields: {
+    title: {
+      type: 'string',
+      required: true
+    },
+    description: {
+      type: 'string',
+    },
+    date: {
+      type: 'date',
+      required: true
+    },
+    publish: {
+      type: 'boolean',
+      required: true
+    },
+    image: {
+      type: 'string',
+      required: false
+    },
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (doc) => `/${doc._raw.flattenedPath}`,
+    },
+    },
+}));
+
+export default makeSource({
+  contentDirPath: './content',
+  documentTypes: [Post],
+});
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,2 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+import { withContentlayer } from 'next-contentlayer2'
+export default withContentlayer({})
